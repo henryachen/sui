542562aa94155485bdf5b0c21161e7cf0f46e784
patrick <patrickkuo@me.com>
2025-02-03 16:26:47 +0000

    minor fix

--- crates/sui-indexer-alt/src/benchmark.rs
+++ crates/sui-indexer-alt/src/benchmark.rs
@@ -52,7 +52,6 @@ pub async fn run_benchmark(
         remote_store_url: None,
         local_ingestion_path: Some(ingestion_path.clone()),
         rpc_api_url: None,
-        basic_auth: None,
     };
 
     let cur_time = Instant::now();
